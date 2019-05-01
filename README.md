3653232eac64c337c64617fe5b8564d0ce8387ad
STEPHEN CHU <stephen.chu530@gmail.com>
2019-05-01 08:15:15 -0700

    chore: initial setup to start the lab

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# lab-07-back-end
+# LAB-07 Back End
+
+# Project Name
+
+City Explorer backend app
+
+**Author**: Stephen Chu,
+**Version**: 1.1.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+This is a class project to return data with user input. The data to be returned is location info, latitude, longitude, and weather info based on user input.
+
+## Getting Started
+
+### Installation:
+From the terminal, run one of the following to make a local copy:
+* ssh: `git clone git@github.com:stephenchu530/lab-07-back-end.git`
+* https: `git clone https://github.com/stephenchu530/lab-07-back-end.git`
+
+### Usage:
+* Change directory into the project directory and load `index.html` in your favorite browser.
+* Or go to: [TBD](TBD)
+
+## Architecture
+  Node.js, dotenv, cors, express, Heroku
+
+## Change Log
+
+* 04-30-2019 11:00 - Application now has a fully-functional express server, with a GET route for the location resource.
+* 04-30-2019 12:00 - Application now has a fully-functional a GET route for the weather resource.
+* 04-30-2019 13:00 - Application now has a fully-functional a error handling.
+* 05-01-2019 09:00 - Stuff
+
+
+## Credits and Collaborations
+* Name [<githubname>](https://github.com/<githubname>)
+* Stephen Chu, [stephenchu530](https://github.com/stephenchu530)
+
+## Time Estimation
+
+  Number and name of feature: Refactor callback to use .map()
+
+  Estimate of time needed to complete: ____
+
+  Start time: ____
+
+  Finish time: ____
+
+  Actual time needed to complete: ____
+
+
+## Usage:
+* Change directory into the project directory and load `index.html` in your favorite browser.
+* Or go to: [TBD](TBD)
+
+## License:
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://github.com/stephenchu530/lab-07-back-end/blob/master/LICENSE)
---
+++ server.js
@@ -0,0 +1,71 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get('/hello', (request, response) => {
+  response.status(200).send('Hello');
+});
+
+app.get('/location', (request, response) => {
+  try {
+    const locationData = require('./data/geo.json');
+    const locationObj = new Location(request.query.data, locationData);
+
+    response.status(200).send(locationObj);
+  } catch (error) {
+    const errorResponse500 = {'status': 500, 'responseText': 'Sorry, something went wrong' };
+
+    response.status(500).send(errorResponse500);
+  }
+});
+
+app.get('/weather', (request, response) => {
+  try {
+    const weatherData = require('./data/darksky.json');
+    const weatherObj = new Weather(weatherData);
+
+    response.status(200).send(weatherObj.dailyForecast);
+  } catch (error) {
+    const errorResponse500 = {'status': 500, 'responseText': 'Sorry, something went wrong' };
+
+    response.status(500).send(errorResponse500);
+  }
+});
+
+
+app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
+
+app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+
+const Location = function(searchQuery, jsonData) {
+  const formattedQuery = jsonData['results'][0]['formatted_address'];
+  const latitude = jsonData['results'][0]['geometry']['location']['lat'];
+  const longitude = jsonData['results'][0]['geometry']['location']['lng'];
+
+  this.search_query = searchQuery;
+  this.formatted_query = formattedQuery;
+  this.latitude = latitude;
+  this.longitude = longitude;
+};
+
+const Weather = function(jsonData) {
+  this.dailyForecast = [...jsonData['daily']['data']];
+
+  forecastSummary.forEach(forecast => {
+    const summary = forecast['summary'];
+    const time = (new Date(forecast['time'] * 1000)).toDateString();
+
+    this.dailyForecast.push({
+      'forecast': summary,
+      'time': time,
+    });
+  });
+  this.dailyForecast.map(forecast => {
+  });
+};
